5a79f573c4a92bf1bcc4cd2b9f731df9f34b8468
meotiam <kayla.kindle@bsu.edu>
2023-10-29 22:57:08 -0400

    Final Commit, UI implimented

--- src/main/java/TranslatorUI.java
+++ src/main/java/TranslatorUI.java
@@ -1,14 +1,9 @@
-
-
 import javax.swing.*;
-
-
 public class TranslatorUI extends NumeralTranslator{
     public void Translator(){
         int number = Integer.parseInt(JOptionPane.showInputDialog("What number would you like to convert to roman numerals?"));
         String result = numeralTranslator(number);
-        String[] playAgain= {"Play Again?", "Exit"};
-        var replay = JOptionPane.showOptionDialog(null,
+        JOptionPane.showMessageDialog(null,"The roman numeral for " + number + " is " + result);
 
     }
 }
